26c51437082f0573f8014e38483f7677b79c9750
Tamas Eros <tamas.eros1@gmail.com>
2019-12-29 14:35:18 +0100

    Help in setup script for raspberry pi

--- setup_account_raspberrypi.sh
+++ setup_account_raspberrypi.sh
@@ -1,3 +1,11 @@
+# prequisites
+# add user, set password, add user to sudoers file, install git, set git config user and email
+# useradd -m user
+# passwd user
+# sudo apt-get install git
+# git config --global user.email "you@example.com"
+# git config --global user.name "Your Name"
+
 SOURCE_DIR=$(dirname "$0")
 
 sudo apt-get --assume-yes install tmux
